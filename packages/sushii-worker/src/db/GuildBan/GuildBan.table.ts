fa1c79b7afc90850573047351ead294c789813e7
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2025-08-07 22:07:15 -0700

    chore: Remove legacy guildban dbs

--- packages/sushii-worker/src/db/GuildBan/GuildBan.table.ts
+++
@@ -1,6 +0,0 @@
-import type { Insertable, Selectable, Updateable } from "kysely";
-import type { AppPublicGuildBans } from "../../infrastructure/database/dbTypes";
-
-export type GuildBanRow = Selectable<AppPublicGuildBans>;
-export type InsertableGuildBanRow = Insertable<AppPublicGuildBans>;
-export type UpdateableGuildBanRow = Updateable<AppPublicGuildBans>;
