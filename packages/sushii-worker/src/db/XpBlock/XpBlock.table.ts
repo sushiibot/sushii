15b62308f4f9e2a5e2f37a1976045d048e4eabbd
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2025-08-07 17:04:52 -0700

    chore: Remove legacy xpblock db

--- packages/sushii-worker/src/db/XpBlock/XpBlock.table.ts
+++
@@ -1,6 +0,0 @@
-import { Insertable, Selectable, Updateable } from "kysely";
-import { AppPublicXpBlocks } from "../../infrastructure/database/dbTypes";
-
-export type XpBlockRow = Selectable<AppPublicXpBlocks>;
-export type InsertableXpBlockRow = Insertable<AppPublicXpBlocks>;
-export type UpdateableXpBlockRow = Updateable<AppPublicXpBlocks>;
